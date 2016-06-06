277bbb7e83d5e9071682a4559945e68c055b8f8c
Hannes Vogt <hannes@havogt.de>
2016-06-06 10:32:01 +0200

    fixed wrong match in db

--- tippspiel16/plugins/hv_tippspiel16/hv_tippspiel16_variables.php
+++ tippspiel16/plugins/hv_tippspiel16/hv_tippspiel16_variables.php
@@ -1,6 +1,6 @@
 <?php
 
-$hv_tippspiel16_club_label = "Bierfamilie";
+$hv_tippspiel16_club_label = "Club";
 
 $hv_tippspiel16_points_for_cup = 10;
 $hv_tippspiel16_points_for_torschuetze = 10;
@@ -8,7 +8,7 @@ $hv_tippspiel16_points_exact = 7;
 $hv_tippspiel16_points_diff = 5;
 $hv_tippspiel16_points_tendency = 3;
 
-$hv_tippspiel16_cup_winner_id = 1;
-$hv_tippspiel16_torschuetze_id = 2;
+$hv_tippspiel16_cup_winner_id = -1;
+$hv_tippspiel16_torschuetze_id = -1;
 
 $hv_tippspiel16_changetime = 15*60; // 15 minutes
